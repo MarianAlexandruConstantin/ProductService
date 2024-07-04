4f21c00881429c666f15d6f09d21b231d1dacf83
MarianAlexandruConstantin <alexandruconstantin.marian@ulbsibiu.ro>
2024-07-04 10:43:09 +0300

    Add project files.

---
+++ ProductService/Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductService.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProductService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private static List<Product> products = new List<Product>
+        {
+            new Product { Id = 1, Name = "Laptop", Price = 999.99M },
+            new Product { Id = 2, Name = "Mouse", Price = 19.99M },
+            new Product { Id = 3, Name = "Keyboard", Price = 49.99M }
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Product>> GetProducts()
+        {
+            return products;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Product> GetProduct(int id)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return product;
+        }
+    }
+}
